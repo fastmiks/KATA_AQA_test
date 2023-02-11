4270e455aee88de190fc5c684dcb303d1b224cbe
miksons-dekstop <fastmiks@gmail.com>
2023-02-11 12:23:12 +0300

    updated code after 1st review

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,13 +4,19 @@ class Calculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter arithmetic operations in the format 'a + b' or 'a - b' or 'a * b' or 'a / b'");
-        System.out.println("Enter one whitespace between operator and operands!\n\nONLY this expression will work: 'a + b'\nThese expressions will generate an error:  'a+b','a +b', 'a+ b', ' a + b ', 'a + b ', ' a + b'");
         System.out.println("Only numbers from 1 to 10 can be used\n");
         while (true) {
             System.out.println("Enter expression:");
+
             String expression = input.nextLine();
-            String[] splitInput = expression.split(" ");
+            String[] splitInput = expression.trim().split("\\s+");
+
+            if (splitInput.length != 3) {
+                splitInput = expression.split("(?<=[+\\-*/])|(?=[+\\-*/])");
+            }
+
             Validate validate = new Validate();
+
             try {
                 validate.validateExpression(splitInput);
                 Calculate calculate = new Calculate();

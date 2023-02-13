ba5c55c99c09cbb81c9a82301961d6cf0a2f4936
miksons-dekstop <fastmiks@gmail.com>
2023-02-13 19:57:28 +0300

    Refactored code to include calc method, and invoke it in main method, had to add a force exit with "System.exit" in calc, so that the app would close after an error is caught

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,31 +1,36 @@
 import java.util.Scanner;
 
 class Calculator {
+    public static String calc(String input) {
+        String[] splitInput = input.trim().split("\\s+");
+
+        if (splitInput.length != 3) {
+            splitInput = input.split("(?<=[+\\-*/])|(?=[+\\-*/])");
+        }
+
+        Validate validate = new Validate();
+
+        try {
+            validate.validateExpression(splitInput);
+            Calculate calculate = new Calculate();
+            return Integer.toString(calculate.calculateResult(splitInput));
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+            System.exit(1);
+        }
+
+        return "";
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter arithmetic operations in the format 'a + b' or 'a - b' or 'a * b' or 'a / b'");
         System.out.println("Only numbers from 1 to 10 can be used\n");
+
         while (true) {
             System.out.println("Enter expression:");
-
             String expression = input.nextLine();
-            String[] splitInput = expression.trim().split("\\s+");
-
-            if (splitInput.length != 3) {
-                splitInput = expression.split("(?<=[+\\-*/])|(?=[+\\-*/])");
-            }
-
-            Validate validate = new Validate();
-
-            try {
-                validate.validateExpression(splitInput);
-                Calculate calculate = new Calculate();
-                System.out.println(calculate.calculateResult(splitInput));
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-
-                return;
-            }
+            System.out.println(calc(expression));
         }
     }
 }
